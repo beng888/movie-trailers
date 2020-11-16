2c98f43ba65a1e6f6ea84e52346c0ff26c68118d
lawrence ardosa <lawrenceardosa@gmail.com>
2020-11-16 22:39:02 +0800

    Updates

---
+++ precache-manifest.cdb669be0e39237cd48015996a6da971.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b8981341ddd09ce3c87dbbb9bb216ef",
+    "url": "/movie-trailers/index.html"
+  },
+  {
+    "revision": "53ffb65ae3998fc1740d",
+    "url": "/movie-trailers/static/css/1.d805e7bd.chunk.css"
+  },
+  {
+    "revision": "e0398e425667ddea7d62",
+    "url": "/movie-trailers/static/css/10.776f714e.chunk.css"
+  },
+  {
+    "revision": "47e747c79354ba124da9",
+    "url": "/movie-trailers/static/css/11.e0bf0ba1.chunk.css"
+  },
+  {
+    "revision": "530c4d561a25dc73fcc0",
+    "url": "/movie-trailers/static/css/5.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "2d566e884c4f9aa2581c",
+    "url": "/movie-trailers/static/css/9.2f14e9d0.chunk.css"
+  },
+  {
+    "revision": "76f918e4361879219cc4",
+    "url": "/movie-trailers/static/css/main.386fb895.chunk.css"
+  },
+  {
+    "revision": "dd43c94561bc32165095",
+    "url": "/movie-trailers/static/js/0.bf846a90.chunk.js"
+  },
+  {
+    "revision": "53ffb65ae3998fc1740d",
+    "url": "/movie-trailers/static/js/1.4c21b7a6.chunk.js"
+  },
+  {
+    "revision": "e0398e425667ddea7d62",
+    "url": "/movie-trailers/static/js/10.dbfc4afe.chunk.js"
+  },
+  {
+    "revision": "47e747c79354ba124da9",
+    "url": "/movie-trailers/static/js/11.15297131.chunk.js"
+  },
+  {
+    "revision": "1256ec19d3a39a57a963",
+    "url": "/movie-trailers/static/js/12.5fd80b7e.chunk.js"
+  },
+  {
+    "revision": "c84e8988ead173568f4e",
+    "url": "/movie-trailers/static/js/13.fa61de5d.chunk.js"
+  },
+  {
+    "revision": "eeff00ebc1c465d8f8da",
+    "url": "/movie-trailers/static/js/2.12d1caec.chunk.js"
+  },
+  {
+    "revision": "530c4d561a25dc73fcc0",
+    "url": "/movie-trailers/static/js/5.16d8aa2a.chunk.js"
+  },
+  {
+    "revision": "be661b3c05c7a6e5da70",
+    "url": "/movie-trailers/static/js/6.27b49fd5.chunk.js"
+  },
+  {
+    "revision": "8d4a884b381c0578714b",
+    "url": "/movie-trailers/static/js/7.aae9d2e6.chunk.js"
+  },
+  {
+    "revision": "030e37e5fcb5508604d8",
+    "url": "/movie-trailers/static/js/8.821d5a80.chunk.js"
+  },
+  {
+    "revision": "2d566e884c4f9aa2581c",
+    "url": "/movie-trailers/static/js/9.e3947ccf.chunk.js"
+  },
+  {
+    "revision": "76f918e4361879219cc4",
+    "url": "/movie-trailers/static/js/main.76fb0543.chunk.js"
+  },
+  {
+    "revision": "5f2360027bd2573f7712",
+    "url": "/movie-trailers/static/js/runtime~main.755f4ed0.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/movie-trailers/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/movie-trailers/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/movie-trailers/static/media/revicons.2feb69cc.eot"
+  }
+]);
